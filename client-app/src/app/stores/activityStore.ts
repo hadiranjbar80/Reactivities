44e9c2307926c12f7f2d2d98cd9637c732a4cc33
hadiranjbar80 <ranjbarhadi314@gmail.com>
2024-06-06 09:46:59 +0330

    End of section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,14 +1,14 @@
 import { makeAutoObservable, runInAction } from "mobx"
 import { Activity } from "../models/activity"
 import agent from "../api/agent";
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 export default class ActivityStore {
     activityRegistery = new Map<string, Activity>();
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
@@ -20,11 +20,11 @@ export default class ActivityStore {
     }
 
     loadActivities = async () => {
+        this.loadingInitial = true;
         try {
             const activities = await agent.Activities.list();
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activityRegistery.set(activity.id, activity)
+                this.setActivity(activity)
             })
             this.setLoadingInitial(false);
         } catch (error) {
@@ -33,31 +33,44 @@ export default class ActivityStore {
         }
     }
 
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if (activity) { 
+            this.selectedActivity = activity;
+            return activity;
+        }
+        else {
+            this.setLoadingInitial(true);
+            try {
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(() => this.selectedActivity = activity);
+                this.setLoadingInitial(false);
+                return activity;
+            } catch (error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistery.set(activity.id, activity)
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityRegistery.get(id);
+    }
+
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state
     }
 
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistery.get(id);
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    openForm = (id?: string) =>{
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
-    }
-    
     createActivity = async (activity: Activity) => {
         this.loading = true;
         activity.id = uuid();
-        try{
+        try {
             await agent.Activities.create(activity);
             runInAction(() => {
                 this.activityRegistery.set(activity.id, activity);
@@ -65,17 +78,17 @@ export default class ActivityStore {
                 this.editMode = false;
                 this.loading = false;
             })
-        }catch(error) {
+        } catch (error) {
             console.log(error);
             runInAction(() => {
                 this.loading = false;
-            })        
+            })
         }
     }
 
     updateActivity = async (activity: Activity) => {
         this.loading = true;
-        try{
+        try {
             await agent.Activities.update(activity);
             runInAction(() => {
                 this.activityRegistery.set(activity.id, activity);
@@ -83,7 +96,7 @@ export default class ActivityStore {
                 this.editMode = false;
                 this.loading = false;
             })
-        }catch(error) {
+        } catch (error) {
             console.log(error);
             runInAction(() => {
                 this.loading = false;
@@ -93,14 +106,13 @@ export default class ActivityStore {
 
     deleteActivity = async (id: string) => {
         this.loading = true;
-        try{
+        try {
             await agent.Activities.delete(id);
             runInAction(() => {
                 this.activityRegistery.delete(id);
-                if(this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
             })
-        }catch (error){
+        } catch (error) {
             console.log(error);
             runInAction(() => {
                 this.loading = false;
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,20 @@
 import { Button, Form, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-
+import { Link, useNavigate, useParams } from "react-router-dom";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { v4 as uuid } from 'uuid';
 
 export default observer(function ActivityForm() {
-  const {activityStore} = useStore();
-  const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore
+  const { activityStore } = useStore();
+  const { createActivity, updateActivity, loading,
+    loadActivity, loadingInitial } = activityStore
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  const initialState = selectedActivity ?? {
+  const [activity, setActivity] = useState<Activity>({
     id: '',
     title: '',
     description: '',
@@ -17,32 +22,40 @@ export default observer(function ActivityForm() {
     category: '',
     city: '',
     venue: ''
-  }
+  });
 
-  const [activity, setActivity] = useState(initialState)
+  useEffect(() => {
+    if (id) loadActivity(id).then(activity => setActivity(activity!))
+  }, [id, loadActivity])
 
-  const handleSubmit = () =>{
-    activity.id ? updateActivity(activity) : createActivity(activity);
-
+  const handleSubmit = () => {
+    if (!activity.id) {
+      activity.id = uuid();
+      createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+    } else {
+      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+    }
   }
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const {name, value} = event.target;
-    setActivity({...activity, [name]: value})
+    const { name, value } = event.target;
+    setActivity({ ...activity, [name]: value })
   }
+
+  if (loadingInitial) return <LoadingComponent content="Loading activity..." />
 
   return (
     <Segment clearing>
-        <Form onSubmit={handleSubmit}>
-            <Form.Input placeholder='Title' value={activity.title} onChange={handleInputChange} name='title' />
-            <Form.TextArea placeholder='Description' value={activity.description} onChange={handleInputChange} name='description' />
-            <Form.Input placeholder='Category' value={activity.category} onChange={handleInputChange} name='category' />
-            <Form.Input type="date" placeholder='Date' value={activity.date} onChange={handleInputChange} name='date' />
-            <Form.Input placeholder='City' value={activity.city} onChange={handleInputChange} name='city' />
-            <Form.Input placeholder='Venue' value={activity.venue} onChange={handleInputChange} name='venue' />
-            <Button loading={loading} floated="right" positive type="submit" content="Submit" />
-            <Button onClick={closeForm} floated="right" type="button" content="Candel" />
-        </Form>
+      <Form onSubmit={handleSubmit}>
+        <Form.Input placeholder='Title' value={activity.title} onChange={handleInputChange} name='title' />
+        <Form.TextArea placeholder='Description' value={activity.description} onChange={handleInputChange} name='description' />
+        <Form.Input placeholder='Category' value={activity.category} onChange={handleInputChange} name='category' />
+        <Form.Input type="date" placeholder='Date' value={activity.date} onChange={handleInputChange} name='date' />
+        <Form.Input placeholder='City' value={activity.city} onChange={handleInputChange} name='city' />
+        <Form.Input placeholder='Venue' value={activity.venue} onChange={handleInputChange} name='venue' />
+        <Button loading={loading} floated="right" positive type="submit" content="Submit" />
+        <Button as={Link} to='/activities' floated="right" type="button" content="Candel" />
+      </Form>
     </Segment>
   )
 })
