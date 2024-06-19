252b90acd64e9a677f20e9a02e00ea3b91ca6dc6
hadiranjbar80 <ranjbarhadi314@gmail.com>
2024-06-19 09:58:24 +0330

    End of section 14

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -10,11 +10,24 @@ namespace Persistence
         {
         }
 
-        public DbSet<Activity> Activities  { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> activityAttendees { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+
+            modelBuilder.Entity<ActivityAttendee>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            modelBuilder.Entity<ActivityAttendee>()
+                .HasOne(u => u.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
         }
     }
 }
